e132dc6bb8836e8f0a5e08efa679e07e6a9cb66d
desde42 <desde42@b4d6ab20-219d-e4c1-b8d5-e97ec591408c>
2010-11-15 13:02:52 +0000

    affichage correcte quelque soit le nombre d'espaces separant les nombres.

--- trunk/Taquin42/Taquin42/main.cpp
+++ trunk/Taquin42/Taquin42/main.cpp
@@ -2,5 +2,11 @@
 
 int main()
 {
-  aff("1 2 3\n8 0 4\n7 6 5\n");
+	std::cout << "Taquin A1:" << std::endl;
+	aff("1 2\n0 3");
+
+	std::cout << std::endl << "Taquin A2:" << std::endl;
+	aff("1 2 3\n8 0 4\n7 6 5\n");
+	std::cout << std::endl << "Taquin A3:" << std::endl;
+	aff("1  2  3  4  5\n16 17 18 19 6\n15 24 0  20 7\n14 23 22 21 8\n13 12 11 10 9\n");
 }
