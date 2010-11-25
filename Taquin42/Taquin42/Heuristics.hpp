e78953b3b9181327a440a03ff22f70d18d1ca858
sikilaxx01 <sikilaxx01@b4d6ab20-219d-e4c1-b8d5-e97ec591408c>
2010-11-25 16:38:53 +0000

    Variables added

---
+++ Taquin42/Taquin42/Heuristics.hpp
@@ -0,0 +1,39 @@
+
+#ifndef _HEURISTICS_HPP_
+# define _HEURISTICS_HPP_
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+class				Heuristics
+{
+public:
+  Heuristics();
+  ~Heuristics();
+  
+private:
+  Heuristics(const Heuristics &);
+  Heuristics &			operator=(const Heuristics &);
+
+public:
+  inline int			Theory(int Node_px, int Node_py);
+  inline const sPositions &	GetCurrentPos(int CurrentNodeName);
+  inline bool			CanMove(sPositions CurrentNodePos, const std::vector< std::vector <int> > & PuzzleMap);
+
+public:
+  virtual int			GetMovesNb(void) const;
+  virtual std::string &		GetStatesTime(void) const;
+  virtual int			GetStatesSize(void) const;
+  virtual int			GetTotalCosts(void) const;
+  virtual std::string &		GetOrderedSequence(void) const;
+
+public:
+  int				MovesNb;
+  std::string			StatesTime;
+  int				StatesSize;
+  int				TotalCosts;
+  std::string			OrderedSequence;
+};
+
+#endif //_HEURISTICS_HPP_
