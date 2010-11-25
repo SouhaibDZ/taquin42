f41fae7db2f162d68e86768296bf8fae70753352
sikilaxx01 <sikilaxx01@b4d6ab20-219d-e4c1-b8d5-e97ec591408c>
2010-11-25 16:38:53 +0000

    Variables added

---
+++ trunk/Taquin42/Taquin42/Variables.hpp
@@ -0,0 +1,16 @@
+#ifndef _VARIABLES_HPP_
+# define _VARIABLES_HPP_
+
+struct	sPositions
+{
+  int	Node_px;
+  int	Node_py;
+};
+
+struct	sPositions
+{
+  int	GoalNode_px;
+  int	GoalNode_py;
+};
+
+#endif // _VARIABLES_HPP_
