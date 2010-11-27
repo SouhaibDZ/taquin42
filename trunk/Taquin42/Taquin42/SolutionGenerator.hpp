5c45da08d9f5fe2033bc2e1642b6769f086f98f2
sikilaxx01 <sikilaxx01@b4d6ab20-219d-e4c1-b8d5-e97ec591408c>
2010-11-27 13:23:15 +0000

    Puzzle.hpp and SolutionGenerator.hpp

---
+++ trunk/Taquin42/Taquin42/SolutionGenerator.hpp
@@ -0,0 +1,33 @@
+#ifndef _SOLUTIONGENERATOR_HPP_
+#define _SOLUTIONGENERATOR_HPP_
+
+class 					SolutionGenerator
+{
+public:
+	SolutionGenerator(const int Scale = 100);
+	~SolutionGenerator();
+
+private:
+	SolutionGenerator&		operator=(SolutionGenerator const&);
+	SolutionGenerator(SolutionGenerator const&);
+
+private:
+	 void				DisplaySolution()const;
+
+public:
+	void				FillTop();
+	void				FillRight();
+	void				FillBottom();
+	void				FillLeft();
+	bool				GenerateSolution();
+
+private:
+	int				X_min, X_max, X_cur;
+	int				Y_min, Y_max, Y_cur;
+	int				Num;
+	int				**Witness;
+	int				PuzzleScale;
+};
+
+#endif /* _SOLUTIONGENERATOR_HPP_ */
+ 
