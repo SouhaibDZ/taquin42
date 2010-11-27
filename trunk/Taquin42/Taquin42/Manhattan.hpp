c430d9f95098253dc22220b9c5fd58325e441973
sikilaxx01 <sikilaxx01@b4d6ab20-219d-e4c1-b8d5-e97ec591408c>
2010-11-27 12:41:29 +0000

    hEURISICS headers

--- trunk/Taquin42/Taquin42/Manhattan.hpp
+++ trunk/Taquin42/Taquin42/Manhattan.hpp
@@ -17,31 +17,25 @@ private:
   Manhattan &			operator=(const Manhattan &);
 
 public:
-  inline int			Theory(const sPostions & NodePos, const sPostions & GoalNodePos)
+  inline int			Theory(const sPostions & NodePos,
+				       const sPostions & GoalNodePos)
   {
-    return (abs(NodePos->Node_px - GoalNodePos->GoalNode_px) + abs(NodePos->Node_py - GoalNodePos->GoalNode_py));
+    return (abs(NodePos->Node_px - GoalNodePos->GoalNode_px) +
+	    abs(NodePos->Node_py - GoalNodePos->GoalNode_py));
   }
   
-  inline const sPositions	GetCurrentPos(int CurrentNodeName)
-  {
-    for (int i = 0; i < PuzzleScale; ++i)
-      {
-	    for (int j = 0; j < PuzzleScale; ++j)
-	      {
-		if ()
-	      }
-      }
-  }
-
   inline bool			CanMove(sPositions CurrentNodePos,
-					const std::vector< std::vector <int> > & PuzzleMap);
+					const int ** PuzzleMap)
+  {
+    return (true);
+  }
 
 public:
   virtual int			GetMovesNb(void) const;
-  virtual std::string &		GetStatesTime(void) const;
+  virtual std::string &	GetStatesTime(void) const;
   virtual int			GetStatesSize(void) const;
   virtual int			GetTotalCosts(void) const;
-  virtual std::string &		GetOrderedSequence(void) const;
+  virtual std::string &	GetOrderedSequence(void) const;
 
 private:
   int				MovesNb;
