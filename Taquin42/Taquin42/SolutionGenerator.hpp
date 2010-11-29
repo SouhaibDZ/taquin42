bc050ab8b5a3b294b79b36b920bf91c0bb82caf5
desde42 <desde42@b4d6ab20-219d-e4c1-b8d5-e97ec591408c>
2010-11-29 16:48:47 +0000

    solution generator in puzzle, now

--- Taquin42/Taquin42/SolutionGenerator.hpp
+++ Taquin42/Taquin42/SolutionGenerator.hpp
@@ -1,32 +1,36 @@
 #ifndef _SOLUTIONGENERATOR_HPP_
 #define _SOLUTIONGENERATOR_HPP_
+#include "Variables.hpp"
 
 class 					SolutionGenerator
 {
 public:
-	SolutionGenerator(const int Scale = 100);
+	SolutionGenerator(unsigned int Scale = 100);
 	~SolutionGenerator();
 
 private:
-	SolutionGenerator&		operator=(SolutionGenerator const&);
+	SolutionGenerator&	operator=(SolutionGenerator const&);
 	SolutionGenerator(SolutionGenerator const&);
 
 private:
-	 void				DisplaySolution()const;
+	 void				DisplaySolution() const;
 
-public:
+private:
 	void				FillTop();
 	void				FillRight();
 	void				FillBottom();
 	void				FillLeft();
+
+public:
 	bool				GenerateSolution();
+	void				SearchNodeGoalPos(int CurrentNodeName, sPositions& pos) const;
 
 private:
-	int				X_min, X_max, X_cur;
-	int				Y_min, Y_max, Y_cur;
-	int				Num;
-	int				**Witness;
-	int				PuzzleScale;
+	int					X_min, X_max, X_cur;
+	int					Y_min, Y_max, Y_cur;
+	int					Num;
+	int					**Witness;
+	unsigned int		PuzzleScale;
 };
 
 #endif /* _SOLUTIONGENERATOR_HPP_ */
