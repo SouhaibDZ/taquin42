c1de69d3f76b8c0272bc920d544267b3b80c185f
desde42 <desde42@b4d6ab20-219d-e4c1-b8d5-e97ec591408c>
2010-12-02 08:54:01 +0000

    debut de l'arbre

--- trunk/Taquin42/Taquin42/Manhattan.cpp
+++ trunk/Taquin42/Taquin42/Manhattan.cpp
@@ -1,3 +1,4 @@
+#include <map>
 #include "Manhattan.hpp"
 
 Manhattan::Manhattan()
@@ -27,6 +28,7 @@ void						Manhattan::SearchNextPosZero(Puzzle& p)
 	sPositions CurrentNodePos;
 	sPositions ZeroNodePos;
 	sPositions CurrentNodeDestinationPos;
+	sPositions ZeroNodeDestinationPos(2,1);
 
 	p.SearchCurrentNodePos(this->CurrentNodeName, CurrentNodePos);
 	p.SearchCurrentNodePos(0, ZeroNodePos);
@@ -41,22 +43,42 @@ void						Manhattan::SearchNextPosZero(Puzzle& p)
 	{
 		//distance of Manhattan
 		//4 possible directions
-		this->Arbre(p.GetMap(), p.GetScale(), ZeroNodePos);
+		this->Arbre(p.GetMap(), p.GetScale(), ZeroNodePos, ZeroNodeDestinationPos);
 		//to check the 4 possibilities
 	}
 }
 
-void					Manhattan::Arbre(int ** Map, unsigned int Size, sPositions Pos)
-{/*
-	if (Pos.Node_px > 0) //left
-		calMan();
-	if (Pos.Node_px < Size) //right
-		calMan();
-	if (Pos.Node_py > 0) //up
-		calMan();
-	if (Pos.Node_py < Size) //down
-		calMan();
-*/
+void					Manhattan::Arbre(int ** Map, unsigned int Size, sPositions NodePos, sPositions& DestinationPos)
+{
+	std::map<void (Manhattan::*)(), int>		dep;
+	int											distance;
+
+	void	(Manhattan::*prout)();
+	prout = &Manhattan::left;
+
+	if (NodePos.Node_px > 0) //left
+	{
+		distance = this->Theory(sPositions(NodePos.Node_px - 1, NodePos.Node_py), DestinationPos);
+		std::cout << "Left Distance : " << distance << std::endl;
+		//dep[&Manhattan::left] = distance;
+		//dep[*prout] = distance;
+	}
+	if (NodePos.Node_px < Size - 1) //right
+	{
+		distance = this->Theory(sPositions(NodePos.Node_px + 1, NodePos.Node_py), DestinationPos);
+		std::cout << "Right  Distance : " << distance << std::endl;
+	}
+	if (NodePos.Node_py > 0) //up
+	{
+		distance = this->Theory(sPositions(NodePos.Node_px, NodePos.Node_py - 1), DestinationPos);
+		std::cout << "Up  Distance : " << distance << std::endl;
+	}
+	if (NodePos.Node_py < Size - 1) //down
+	{
+		distance = this->Theory(sPositions(NodePos.Node_px, NodePos.Node_py + 1), DestinationPos);
+		std::cout << "Down  Distance : " << distance << std::endl;
+	}
+	system("pause");
 // Compute Manhattan distance from the closest cases
 // Next, choose way and sort this choose
 	/*
@@ -65,12 +87,30 @@ void					Manhattan::Arbre(int ** Map, unsigned int Size, sPositions Pos)
 	left(); 4 
 	right(); 5*/
 }
-/*
-void up()
+
+void						Manhattan::up()
 {
 	//stocker le chemin ds une liste
-	arbre();
-}*/
+	//arbre();
+}
+
+void						Manhattan::down()
+{
+	//stocker le chemin ds une liste
+	//arbre();
+}
+
+void						Manhattan::left()
+{
+	//stocker le chemin ds une liste
+	//arbre();
+}
+
+void						Manhattan::right()
+{
+	//stocker le chemin ds une liste
+	//arbre();
+}
 
 void						Manhattan::showDirection()
 {
--- trunk/Taquin42/Taquin42/Variables.cpp
+++ trunk/Taquin42/Taquin42/Variables.cpp
@@ -1,10 +1,10 @@
 #include "Variables.hpp"
 
-sPositions::sPositions() : Node_px(0), Node_py(0)
+sPositions::sPositions() : Node_px(0), Node_py(0), Block(false)
 {
 }
 
-sPositions::sPositions(int x, int y) : Node_px(x), Node_py(y)
+sPositions::sPositions(int x, int y) : Node_px(x), Node_py(y), Block(false)
 {
 }
 
