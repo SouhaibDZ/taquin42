4740968dc259027cfd4d04a6c055bf170e080976
desde42 <desde42@b4d6ab20-219d-e4c1-b8d5-e97ec591408c>
2010-12-13 22:42:16 +0000

    Notre appli trouve la solution, il reste juste à automatiser tout ça

--- trunk/taquin Astar/Taquin Astar/main.cpp
+++ trunk/taquin Astar/Taquin Astar/main.cpp
@@ -20,58 +20,71 @@ int main()
 	F.LoadFile("TaquinA4.txt", S);
 	std::istringstream		In(S);
 	Tab = P.CreatePuzzle(S);
-	int x, y = 0;
+
+	int x = 0, y = 0;
 	short unsigned int node = 10;
 	std::string Dir;
+	std::list<Puzzle>::iterator FirstPuzzle;
 	//std::cout << "Node Possibilities : " << node << " "<< P.LookDirection(Tab, x, y, node, Dir) << std::endl;
 //P.CreatePossiblities(Tab, x, y, node, Dir);
 	std::cout << std::endl;
 	OpenedList.push_back(P);
-	if (!OpenedList.empty())
+	for (int i = 0; i < 120000; ++i)
 	{
-		std::list<Puzzle>::iterator it = OpenedList.begin();
-		if (P.CanUp())
+		if (!OpenedList.empty())
 		{
-			Puzzle PUp(P); // on cree la copie du puzzle
-			PUp.ExecUp(); //on decale le 0 vers le haut
-			AddInList(OpenedList, ClosedList, PUp);
-		}
-		if (P.CanDown())
-		{
-			Puzzle PDown(P);
-			PDown.ExecDown();
-			AddInList(OpenedList, ClosedList, PDown);
-		}
-		if (P.CanLeft())
-		{
-			Puzzle PLeft(P);
-			PLeft.ExecLeft();
-			AddInList(OpenedList, ClosedList, PLeft);
-		}
-		if (P.CanRight())
-		{
-			Puzzle PRight(P);
-			PRight.ExecRight();
-			AddInList(OpenedList, ClosedList, PRight);
-		}
-			
-		// transfer du premiere element de la liste open
-		// vers la fin de la liste closed
-		ClosedList.push_back(*it);
-		OpenedList.erase(it);
+			FirstPuzzle = OpenedList.begin();
+			if ((*FirstPuzzle).GetManhattanDistance() == 0)
+			{
+				std::cout << "Résultat trouvé au bout de " << i + 1 << " iterations" << std::endl; 
+				std::cout << "Size Opened: " << OpenedList.size() << std::endl;
+				std::cout << "Size Closed: " << ClosedList.size() << std::endl;
+				(*FirstPuzzle).ManhattanDistance();
+				(*FirstPuzzle).AffPuzzle();
+				system("pause");
+			}
+			if ((*FirstPuzzle).CanUp())
+			{
+				Puzzle PUp((*FirstPuzzle)); // on cree la copie du puzzle
+				PUp.ExecUp(); //on decale le 0 vers le haut
+				AddInList(OpenedList, ClosedList, PUp);
+			}
+			if ((*FirstPuzzle).CanDown())
+			{
+				Puzzle PDown((*FirstPuzzle));
+				PDown.ExecDown();
+				AddInList(OpenedList, ClosedList, PDown);
+			}
+			if ((*FirstPuzzle).CanLeft())
+			{
+				Puzzle PLeft((*FirstPuzzle));
+				PLeft.ExecLeft();
+				AddInList(OpenedList, ClosedList, PLeft);
+			}
+			if ((*FirstPuzzle).CanRight())
+			{
+				Puzzle PRight((*FirstPuzzle));
+				PRight.ExecRight();
+				AddInList(OpenedList, ClosedList, PRight);
+			}
+				
+			// transfer du premiere element de la liste open
+			// vers la fin de la liste closed
+			ClosedList.push_back(*FirstPuzzle);
+			OpenedList.erase(FirstPuzzle);
 
-		//Debug
-		std::list<Puzzle>::iterator it2 = OpenedList.begin();
-		std::list<Puzzle>::iterator it2_end = OpenedList.end();
-		std::cout << std::endl << "Opened List:" << std::endl;
-		while (it2 != it2_end)
-		{
-			std::cout << (*it2).GetManhattanDistance() << ",";
-			++it2;
+			//Debug
+			/*std::list<Puzzle>::iterator it2 = OpenedList.begin();
+			std::list<Puzzle>::iterator it2_end = OpenedList.end();
+			std::cout << std::endl << "Opened List:" << std::endl;
+			while (it2 != it2_end)
+			{
+				std::cout << (*it2).GetManhattanDistance() << ",";
+				++it2;
+			}
+			std::cout << std::endl;*/
 		}
-		std::cout << std::endl;
 	}
-
 	return (0);
 }
 
